f1f4092973de0b6aba0c0e76e7448663480d294e
Janisa <janisasmobil@icloud.com>
2025-11-27 13:03:48 +0100

    Added product card

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 import "./assets/scss/style.scss";
+
+const container = document.querySelector<HTMLDivElement>("#product-list");
+
+const products = [
+  {
+    image: "bild1.jpg",
+    title: "Produkt 1",
+    description: "Beskrivning 1"
+  },
+];
+
+products.forEach(product => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <img class="card-img-top" src="${product.image}" alt="${product.title}">
+    <div class="card-body">
+      <h5 class="card-title">${product.title}</h5>
+      <p class="card-text">${product.description}</p>
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-primary">Köp</button>
+      <button class="btn btn-secondary">Mer info</button>
+    </div>
+  `;
+
+  container?.appendChild(card);
+});
