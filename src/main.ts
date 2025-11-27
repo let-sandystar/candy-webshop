b338478fa7a1070855a70d5444c026303db28a41
Janisa <janisasmobil@icloud.com>
2025-11-27 16:36:26 +0100

    Fetch products with getAllProducts, loop through them

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import "./assets/scss/style.scss";
-import { getAllProducts } from "./services/allproducts"
 
 const container = document.querySelector<HTMLDivElement>("#product-list");
 
@@ -12,15 +11,15 @@ const products = [
   },
 ];
 
-products.forEach(getAllProducts => {
+products.forEach(product => {
   const card = document.createElement("div");
   card.classList.add("card");
 
   card.innerHTML = `
-    <img class="card-img-top" src="${products.image}" alt="${products.title}">
+    <img class="card-img-top" src="${product.image}" alt="${product.title}">
     <div class="card-body">
-      <h5 class="card-title">${products.title}</h5>
-      <p class="card-text">${products.description}</p>
+      <h5 class="card-title">${product.title}</h5>
+      <p class="card-text">${product.description}</p>
     </div>
     <div class="card-footer">
       <button class="btn btn-primary">Köp</button>
--- src/services/allproducts.ts
+++ src/services/allproducts.ts
@@ -3,20 +3,17 @@ import type { everyProduct } from "./candy.types";
 const BASE = import.meta.env.VITE_API_BASE_URL;
 const PRODUCTS_URL = `${BASE}api/v2/products`;
 
-
 export const getAllProducts = async () => {
-    if(!PRODUCTS_URL) {
-        throw new Error("No api found");
+    if (!PRODUCTS_URL) {
+        throw new Error("No API found");
     }
 
-    const res = await fetch(`${PRODUCTS_URL}`);
+    const res = await fetch(PRODUCTS_URL);
 
-    if(!res.ok){
+    if (!res.ok) {
         throw new Error(`${res.status} ${res.statusText}`);
     }
 
     const allProducts = await res.json() as everyProduct;
-
-    return allProducts; 
-    console.log(`${allProducts}`);
+    return allProducts;
 };
