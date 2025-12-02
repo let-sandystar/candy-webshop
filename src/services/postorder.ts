4a5107f4489aca8e6469de29e636d839f1f6f8a7
Sandra Coco <sandrac94@hotmail.com>
2025-12-02 11:59:59 +0100

    finished catch error function after fixing typos and adding return etc for api post module

--- src/services/postorder.ts
+++ src/services/postorder.ts
@@ -1,12 +1,19 @@
 import type { orderPayLoad,orderResponse } from "./candy.types";
 const POST = import.meta.env.VITE_API_ORDER_URL;
 
-const postOrder = async (orderPayLoad: orderPayLoad):Promise<orderResponse> => {
+export const postOrder = async (orderPayLoad: orderPayLoad):Promise<orderResponse> => {
     try {
-        const res = await fetch(`${POST}`), {
+        const res = await fetch(`${POST}`, {
             method: "POST",
             headers: { "Content-Type":"application/json" },
             body: JSON.stringify(orderPayLoad)
-        }
+        });
+        
+        if(!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+
+        return await res.json() as orderResponse;
+    } catch(err) {
+        console.error("Fel vid order", err);
+        throw err;
     }
-}
+};
